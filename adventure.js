df7de5fe48822e10f701bf17ebec8da4ca13d329
Michael <xxtwomadxx@gmail.com>
2023-05-12 21:33:50 -0700

    extended padding even more

--- adventure.js
+++ adventure.js
@@ -62,7 +62,7 @@ class AdventureScene extends Phaser.Scene {
             .setFontSize(this.s * scale)
             .setOrigin(0.5, 0.5)
             .setInteractive()
-            .setPadding(50)
+            .setPadding(100)
         return item;
     }
 
